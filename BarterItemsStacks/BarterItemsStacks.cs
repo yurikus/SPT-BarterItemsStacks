c1e3f5b9e6e139b40bde75272e5fde52010ba8ee
slpf <markdmitre@gmail.com>
2025-12-08 13:50:34 +0700

    change version in metadata

--- BarterItemsStacks/BarterItemsStacks.cs
+++ BarterItemsStacks/BarterItemsStacks.cs
@@ -17,7 +17,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "Barter Items Stacks";
     public override string Author { get; init; } = "SLPF";
     public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+    public override SemanticVersioning.Version Version { get; init; } = new("1.1.2");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
@@ -60,4 +60,5 @@ public class AfterDBLoadHook(
 
         return Task.CompletedTask;
     }
+
 }
